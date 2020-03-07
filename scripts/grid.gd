f36504eeb703573c1b146f217db85fa9f025d823
nicolosponziello <nicolosponziello@gmail.com>
2020-03-07 16:03:48 +0100

    Project setup and working grid

---
+++ scripts/grid.gd
@@ -0,0 +1,135 @@
+extends Node2D
+
+# Grid Variables
+export (int) var width;
+export (int) var height;
+export (int) var x_start;
+export (int) var y_start;
+export (int) var offset;
+
+#the piece array
+var possible_pieces = [
+	preload("res://scenes/yellow_piece.tscn"),
+	preload("res://scenes/blue_piece.tscn"),
+	preload("res://scenes/pink_piece.tscn"),
+	preload("res://scenes/green_piece.tscn"),
+	preload("res://scenes/l_green_piece.tscn"),
+	preload("res://scenes/orange_piece.tscn")	
+];
+
+var all_pieces = [];
+
+# Touch
+var first_touch = Vector2(0,0);
+var final_touch = Vector2(0,0);
+var controlling = false;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	randomize();
+	all_pieces = make2dArray();
+	spawn_pices();
+
+func make2dArray():
+	var array = [];
+	for i in width:
+		array.append([]);
+		for j in height:
+			array[i].append(null);
+	return array;
+
+func grid_to_pixel(col, row):
+	var new_x = x_start + offset * col;
+	var new_y = y_start + -offset * row;
+	return Vector2(new_x, new_y);
+
+func spawn_pices():
+	for i in width:
+		for j in height:
+			var random = generate_random();
+			#create the piece
+			var piece = possible_pieces[random].instance();
+			while(match_at(i, j, piece.color)):
+				random = generate_random();
+				piece = possible_pieces[random].instance();
+			add_child(piece);
+			piece.position = grid_to_pixel(i, j);
+			all_pieces[i][j] = piece;
+			
+func match_at(col, row, color):
+	if col > 1:
+		if all_pieces[col - 1][row] != null && all_pieces[col - 2][row] != null:
+			if all_pieces[col-1][row].color == color && all_pieces[col -2][row].color == color:
+				return true;
+	if row > 1:
+		if all_pieces[col][row-1] != null && all_pieces[col][row-2] != null:
+			if all_pieces[col][row-1].color == color && all_pieces[col][row-2].color == color:
+				return true;
+	return false;
+				
+func generate_random():
+	return floor(rand_range(0, possible_pieces.size()));
+	
+func pixel_to_grid(x, y):
+	print("x: " + String(x));
+	print("y: " + String(y));
+	var col = round((x - x_start) / offset);
+	var row = round((y - y_start) / -offset);
+	if row == -0:
+		row = 0;
+	if col == -0:
+		col = 0;
+	return Vector2(col, row);
+	
+func is_in_grid(col, row):
+	if col >= 0 && col < width:
+		if row >= 0 && row < height:
+			return true;
+	return false;
+	
+func touch_input():
+	if Input.is_action_just_pressed("ui_touch"):
+		var touch_pos = get_global_mouse_position();
+		touch_pos = pixel_to_grid(touch_pos.x, touch_pos.y);
+		if is_in_grid(touch_pos.x, touch_pos.y):
+			controlling = true;
+			first_touch = touch_pos;
+			print(first_touch);
+		
+	if Input.is_action_just_released("ui_touch"):
+		var touch_pos_released = get_global_mouse_position();
+		touch_pos_released = pixel_to_grid(touch_pos_released.x, touch_pos_released.y);
+		if(is_in_grid(touch_pos_released.x, touch_pos_released.y) && controlling && !check_same_piece(first_touch, touch_pos_released)):
+			final_touch = touch_pos_released;			
+			touch_diff(first_touch, final_touch);
+			controlling = false;
+			
+func check_same_piece(pos1, pos2):
+	return pos1.x == pos2.x && pos1.y == pos2.y;
+
+func swap_pieces(col, row, direction):
+	var first = all_pieces[col][row];
+	var second = all_pieces[col + direction.x][row + direction.y];
+	all_pieces[col][row] = second;
+	all_pieces[col + direction.x][row + direction.y] = first;
+	first.position = grid_to_pixel(col + direction.x, row + direction.y);
+	second.position = grid_to_pixel(col, row);
+	
+func touch_diff(grid_1, grid_2):
+	print("Grid_1" + String(grid_1));
+	print("Grid_2" + String(grid_2));	
+	var diff = grid_2 - grid_1;
+	print("touch_diff" + String(diff));
+	if abs(diff.x) > abs(diff.y):
+		if diff.x > 0:
+			swap_pieces(grid_1.x, grid_1.y, Vector2(1, 0));
+		elif diff.x < 0:
+			swap_pieces(grid_1.x, grid_1.y, Vector2(-1, 0));
+	elif abs(diff.y) > abs(diff.x):
+		if diff.y > 0:
+			swap_pieces(grid_1.x, grid_1.y, Vector2(0, 1));
+		elif diff.y < 0:
+			swap_pieces(grid_1.x, grid_1.y, Vector2(0, -1));
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	touch_input();
